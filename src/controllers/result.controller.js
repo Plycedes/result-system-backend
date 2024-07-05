5cbe9ecf484ec68f5e6d15e200874a3f214b3525
Plycedes <abhaypratapyadav340@gmail.com>
2024-07-05 18:29:38 +0530

    Get results

--- src/controllers/result.controller.js
+++ src/controllers/result.controller.js
@@ -3,16 +3,22 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { Result } from "../models/result.model.js";
 
-const setResult = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "I love Jagriti",
-  });
-});
-
 const getResult = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "I love Jagriti",
+  const { rollno, mobileno } = req.body;
+
+  if (!rollno || !mobileno) {
+    throw new ApiError(400, "Both Mobile No and Roll No are required");
+  }
+
+  const result = await Result.findOne({
+    $and: [{ mobileno }, { rollno }],
   });
+
+  if (!result) {
+    throw new ApiError(404, "Result does not exist");
+  }
+
+  return res.status(200).json(new ApiResponse(200, result, "Result found"));
 });
 
 export { getResult };
